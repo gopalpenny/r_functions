8010e1cb39cb526b27a277f1202e9cfe295cb4cc
Gopal Penny <gopalpenny@gmail.com>
2014-05-01 19:23:10 -0700

    minor change

--- plotobj.R
+++ plotobj.R
@@ -1,5 +1,7 @@
 #Plotting tools and color pallettes
 
+require(grid)
+
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 ### viewports. with ggplot use, e.g., print(p,vp$TL)
